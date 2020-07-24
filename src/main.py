b1d645bb228496be62bbfc772bfe3b4bd59bb7a3
J. Alvarez <jcalvarezj@gmail.com>
2020-07-24 16:20:45 -0500

    Solved challenge

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 # Resolve the problem!!
+import re
+
+FILE_NAME = 'encoded.txt'
 
 
 def run():
-    # Start coding here
+    with open(FILE_NAME, encoding = 'utf-8') as input_file:
+        contents = input_file.read()
+        message = ''.join(re.findall(r'[a-z]', contents))
+        print(message)
 
 
 if __name__ == '__main__':
